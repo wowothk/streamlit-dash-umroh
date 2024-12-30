a0c37d38e4266aab78b0c4721aaa17d824f21971
wowot_hk <rudi.sarwiyana@yahoo.com>
2024-12-31 02:39:35 +0700

    add information

--- src/app.py
+++ src/app.py
@@ -1,15 +1,17 @@
-# pylint: disable=missing-module-docstring, missing-function-docstring
+# pylint: disable=missing-module-docstring, missing-function-docstring, line-too-long
 import streamlit as st
 from components.home import home
 from components.trend import trend
 from components.market import market
 
 # Set Streamlit to wide mode
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="Dashboard Umroh", page_icon=":kaaba:")
 
 def main():
     st.sidebar.title("Dashboard Umroh")
-    st.sidebar.write("Made with ❤️ by [Hamba Allah]")
+    st.sidebar.write(
+        "This dashboard is created to visualize the condition, trend, and market of Umroh in Indonesia."
+    )
 
      # Navigation
     page = st.sidebar.radio("Please select this", ["Condition", "Trend", "Market"])
