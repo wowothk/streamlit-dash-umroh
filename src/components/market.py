1f416b942655702730d78550e8f88e1fb8a8fa2e
wowot_hk <rudi.sarwiyana@yahoo.com>
2024-12-31 02:03:25 +0700

    fix: unused library

--- src/components/market.py
+++ src/components/market.py
@@ -1,5 +1,4 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring, line-too-long
-import streamlit as st
 from streamlit_echarts import st_echarts
 from utils.load_data import load_data
 
